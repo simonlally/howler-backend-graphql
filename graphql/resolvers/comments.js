1eb67a491cbef08b40de3d21ec9b1b800c8ce911
Simon Lally <simonlallydev@gmail.com>
2019-08-20 15:43:34 -0400

    create and delete comment mutations

---
+++ graphql/resolvers/comments.js
@@ -0,0 +1,50 @@
+const Post = require("../../models/Post");
+const { UserInputError, AuthenticationError } = require("apollo-server");
+const validateAuth = require("../../util/validateAuth");
+
+module.exports = {
+  Mutation: {
+    createComment: async (_, { postId, body }, context) => {
+      const { username } = validateAuth(context);
+      if (body.trim() === "") {
+        throw new UserInputError("Empty comment", {
+          errors: {
+            body: "comment body cannot be empty"
+          }
+        });
+      }
+      const post = await Post.findById(postId);
+
+      if (post) {
+        post.comments.unshift({
+          body,
+          username,
+          createdAt: new Date().toISOString()
+        });
+        await post.save();
+        return post;
+      } else {
+        throw new UserInputError("Post not found");
+      }
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = validateAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex(c => c.cid === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else {
+          throw new AuthenticationError("not allowed");
+        }
+      } else {
+        throw new UserInputError("post not found");
+      }
+    }
+  }
+};
--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const postsResolvers = require("./posts.js");
 const usersResolvers = require("./users.js");
+const commentsResolvers = require("./comments");
 
 module.exports = {
   Query: {
@@ -7,6 +8,7 @@ module.exports = {
   },
   Mutation: {
     ...usersResolvers.Mutation,
-    ...postsResolvers.Mutation
+    ...postsResolvers.Mutation,
+    ...commentsResolvers.Mutation
   }
 };
