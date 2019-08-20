5b59b31d2f45025d5011f3a877d74d248c11dc9e
Simon Lally <simonlallydev@gmail.com>
2019-08-20 11:59:10 -0400

    authentication middleware, createPost mutation

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,4 +1,5 @@
 const Post = require("../../models/Post");
+const validateAuth = require("../../util/validateAuth");
 
 module.exports = {
   Query: {
@@ -9,6 +10,34 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+
+    async getPost(_, { postId }) {
+      try {
+        const post = await Post.findById(postId).sort({ createdAt: -1 });
+        if (post) {
+          return post;
+        } else {
+          throw new Error("post not found");
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  },
+  Mutation: {
+    async createPost(_, { body }, context) {
+      const user = validateAuth(context);
+
+      const newPost = new Post({
+        body,
+        user: user.id,
+        username: user.username,
+        createdAt: new Date().toISOString()
+      });
+
+      const post = await newPost.save();
+      return post;
     }
   }
 };
