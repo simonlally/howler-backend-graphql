71dcf656090af0c4f7c0b38eb3f0d8542a83b53a
Simon Lally <simonlallydev@gmail.com>
2019-09-05 15:10:42 -0400

    fixed type in comment resolver

--- graphql/resolvers/comments.js
+++ graphql/resolvers/comments.js
@@ -27,13 +27,13 @@ module.exports = {
         throw new UserInputError("Post not found");
       }
     },
-    async deleteComment(_, { postId, commentId }, context) {
+    deleteComment: async (_, { postId, commentId }, context) => {
       const { username } = validateAuth(context);
 
       const post = await Post.findById(postId);
 
       if (post) {
-        const commentIndex = post.comments.findIndex(c => c.cid === commentId);
+        const commentIndex = post.comments.findIndex(c => c.id === commentId);
 
         if (post.comments[commentIndex].username === username) {
           post.comments.splice(commentIndex, 1);
